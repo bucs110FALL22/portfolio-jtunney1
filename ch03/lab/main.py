9a6a1c78a9eb598d68ecd017207a41fdcb85f749
Justin Tunney <112041098+jtunney1@users.noreply.github.com>
2022-09-21 22:44:19 +0000

    finished ch03 lab

--- ch03/lab/main.py
+++ ch03/lab/main.py
@@ -4,7 +4,6 @@ import pygame
 import math
 
 pygame.init()
-window = pygame.display.set_mode()
 
 #Part A
 window = turtle.Screen() # 2.  Create a screen
@@ -28,22 +27,15 @@ random_a = random.randint(1,101)
 random_b = random.randint(1,101)
 
 #first race
-michelangelo.down() 
-leonardo.down()
 michelangelo.forward(random_a)
 leonardo.forward(random_b)
-pygame.time.wait(1000)
+pygame.time.wait(500)
 
 #reset turtles to start
-michelangelo.up() 
-leonardo.up()
 michelangelo.goto(-100,20)
 leonardo.goto(-100,-20)
 
 #second race
-michelangelo.down() 
-leonardo.down()
-
 for i in range(10):
   michelangelo.forward(random.randint(1,11))
   leonardo.forward(random.randint(1,11))
@@ -59,31 +51,112 @@ leonardo.up()
 michelangelo.goto(-100,20)
 leonardo.goto(-100,-20)
 
+window.exitonclick()
+
 # PART B - complete part B here
 
+#triangle
 #(Step 1) Declare variables
+window = pygame.display.set_mode()
 coords = []
-num_sides = 6
+num_sides = 3
 side_length = 50
-offset = 10
+offset = 100
 
 #(Step 2) generates the points for polygon
-for i in range(side_length):
+for i in range(num_sides):
   theta = (2.0 * math.pi * (i)) / num_sides
   x = side_length * math.cos(theta) + offset
   y = side_length * math.sin(theta) + offset
-  coords.append(x)
-  coords.append(y)
+  coords.append([x , y])
 
 #(Step 3) 
 pygame.draw.polygon(window, 'blue', coords)
 pygame.display.flip()
-pygame.time.wait(500)
+pygame.time.wait(1000)
 
 #reset screen
-window.fill('green')
+window.fill('black')
 pygame.display.flip()
 
 
+#square
+window = pygame.display.set_mode()
+coords = []
+num_sides = 4
+side_length = 50
+offset = 100
 
-window.exitonclick()
+for i in range(num_sides):
+  theta = (2.0 * math.pi * (i)) / num_sides
+  x = side_length * math.cos(theta) + offset
+  y = side_length * math.sin(theta) + offset
+  coords.append([x , y])
+
+pygame.draw.polygon(window, 'blue', coords)
+pygame.display.flip()
+pygame.time.wait(1000)
+
+window.fill('black')
+pygame.display.flip()
+
+#hexagon
+window = pygame.display.set_mode()
+coords = []
+num_sides = 6
+side_length = 50
+offset = 100
+
+for i in range(num_sides):
+  theta = (2.0 * math.pi * (i)) / num_sides
+  x = side_length * math.cos(theta) + offset
+  y = side_length * math.sin(theta) + offset
+  coords.append([x , y])
+
+pygame.draw.polygon(window, 'blue', coords)
+pygame.display.flip()
+pygame.time.wait(1000)
+
+window.fill('black')
+pygame.display.flip()
+
+#nonagon
+window = pygame.display.set_mode()
+coords = []
+num_sides = 9
+side_length = 50
+offset = 100
+
+for i in range(num_sides):
+  theta = (2.0 * math.pi * (i)) / num_sides
+  x = side_length * math.cos(theta) + offset
+  y = side_length * math.sin(theta) + offset
+  coords.append([x , y])
+
+pygame.draw.polygon(window, 'blue', coords)
+pygame.display.flip()
+pygame.time.wait(1000)
+
+window.fill('black')
+pygame.display.flip()
+
+#circle
+window = pygame.display.set_mode()
+coords = []
+num_sides = 360
+side_length = 50
+offset = 100
+
+for i in range(num_sides):
+  theta = (2.0 * math.pi * (i)) / num_sides
+  x = side_length * math.cos(theta) + offset
+  y = side_length * math.sin(theta) + offset
+  coords.append([x , y])
+
+pygame.draw.polygon(window, 'blue', coords)
+pygame.display.flip()
+pygame.time.wait(1000)
+
+window.fill('black')
+pygame.display.flip()
+
