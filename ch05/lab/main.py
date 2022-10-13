3a43bbb551a287a8b088739b41efa951a9e97438
Justin Tunney <112041098+jtunney1@users.noreply.github.com>
2022-10-13 18:22:32 +0000

    completed ch05 lab

---
+++ ch05/lab/main.py
@@ -0,0 +1,38 @@
+import pygame
+
+pygame.init()
+display = pygame.display.set_mode()
+
+dictonary = {}
+numbers = []
+max_so_far_count = 0
+scale = 3
+upperlimit = 100
+for i in range(2,upperlimit):
+  count = 0
+  x = i
+  while x > 1:
+      count = count + 1
+      if x % 2 == 0:
+       x = x/2
+      else:
+       x = x * 3 + 1
+      numbers.append(x)
+  dictonary[i] = count
+  if count > max_so_far_count:
+     max_so_far_num = count
+     max_so_far = str(count)
+     max_val = str(i)
+print(dictonary)
+list = [(k * scale, v * scale) for k, v in dictonary.items()]
+pygame.draw.lines(display, 'white', False, list)
+new_display = pygame.transform.flip(display, False, True)
+display.blit( new_display , (0, 0) )
+font = pygame.font.Font(None, 24)
+text = 'Max count so far is: ' + max_so_far + ' Max value so far is: ' + max_val
+msg = font.render(text, True, 'white')
+display.blit(msg, (10,10))
+pygame.display.flip()
+pygame.time.wait(7000)
+
+
