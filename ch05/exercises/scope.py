75eeba1e3c47e45fc7293c415e4e1df70686d2b1
Justin Tunney <112041098+jtunney1@users.noreply.github.com>
2022-10-19 20:55:50 +0000

    finished ch05 exercise scope

---
+++ ch05/exercises/scope.py
@@ -0,0 +1,17 @@
+def multiply(x=0,y=0):
+  num = 0
+  for i in range(y):
+    num = num + x
+  return num
+def exponent(x=0,y=0):
+  num = 1
+  for i in range(y):
+    num = num * x
+  return num
+def square(num=0):
+  square = multiply(x=num,y=num)
+  return square
+
+print(multiply(5,7))
+print(exponent(3,4))
+print(square(3))
