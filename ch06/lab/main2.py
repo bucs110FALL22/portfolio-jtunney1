eee678272042ae7acfc206519f6312943a518fbf
Justin Tunney <112041098+jtunney1@users.noreply.github.com>
2022-10-26 20:17:08 +0000

    worked on midterm

--- ch06/lab/main2.py
+++ ch06/lab/main2.py
@@ -1,3 +1,72 @@
 import turtle
 #snowman
 
+def background(screen):
+  x_size = 700
+  y_size = 800
+  screen.screensize(x_size,y_size,'light blue')
+
+def snowball_size(ball_radius=0):
+  ball_radius = ball_radius * .8
+  return ball_radius
+
+def snowball_start(y_ball_start=0,ball_radius=0):
+  y_ball_start = y_ball_start + ball_radius * 1.5
+  return y_ball_start
+
+def snowman_body(turtle):
+  x_start = 0
+  y_start = -400
+  radius = 100
+  turtle.color('white')
+
+  for i in range(3):
+   turtle.up()
+   turtle.goto(x_start,y_start)
+   turtle.down()
+   turtle.begin_fill()
+   turtle.circle(radius)
+   y_start = snowball_start(y_ball_start=y_start,ball_radius=radius)
+   radius = snowball_size(ball_radius=radius)
+   turtle.end_fill()
+  
+  
+
+def snowman_face():
+  pass
+
+def snowman_buttons():
+  x_button = 0
+  y_button = -210
+  radius = 3
+  turtle.color('black')
+  turtle.begin_fill()
+  for i in range(3):
+    turtle.up()
+    turtle.goto(x_button,y_button)
+    turtle.down()
+    turtle.circle(radius)
+    y_button = y_button + 45
+  turtle.end_fill()
+    
+
+def snowman_hat():
+  pass
+
+def snowman_arms():
+  pass
+
+def snowflake():
+  pass
+
+def main():
+  my_turtle = turtle.Turtle() 
+  my_turtle.shape('turtle')
+  my_turtle.speed(0)
+  screen = turtle.Screen()
+  background(screen)
+  snowman_body(my_turtle)
+  snowman_buttons()
+
+  screen.exitonclick()
+main()
