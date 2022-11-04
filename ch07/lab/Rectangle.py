8bea6088e743e1f1f618d9ecfd8aa2f1f23b783d
Justin Tunney <112041098+jtunney1@users.noreply.github.com>
2022-11-04 20:24:13 +0000

    ch07 lab done

--- ch07/lab/Rectangle.py
+++ ch07/lab/Rectangle.py
@@ -1,5 +1,8 @@
 class Rectangle:
-  def __init__(self, x, y, h, w):
+  def __init__(self, x=0, y=0, h=0, w=0):
+    #takes arguments and sets instance variables with those values
+	  #args: x,y,h,w integers 
+	  #return: set self.value as argument value
     self.x = x
     self.y = y
     self.height = h
@@ -13,6 +16,9 @@ class Rectangle:
     if self.width < 0:
       self.width = 0
   def __str__(self):
+    #returns values of __init__ function
+	  #args: self only
+	  #return: returns values of self.x,self.y,self.height, and self.width
     string = str.format("(x:{}, y:{}), height:{}, width{}".format(self.x,self.y, self.height,self.width))
     return string
 
--- ch07/lab/Surface.py
+++ ch07/lab/Surface.py
@@ -1,11 +1,17 @@
 from Rectangle import Rectangle
 
 class Surface:
-  def __init__(self,filename,x,y,h,w):
+  def __init__(self,filename=" ",x=0,y=0,h=0,w=0):
+    #takes arguments creates instance variable for rectangle with x,y,h,w. Also sets self.image to argument filename
+	  #args: x,y,h,w integers and filename which is a string
+	  #return: sets self.rect and self.image instance variable values
     self.rect = Rectangle(x,y,h,w)
     self.image = str(filename)
 
   def getRect(self):
+    #returns the self.rect value
+	  #args: self
+	  #return: self.rect
     rect = self.rect
     return rect
     
