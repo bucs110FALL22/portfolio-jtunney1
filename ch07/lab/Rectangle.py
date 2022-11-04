b658d3670a9b625fa060eb2071576117ea7236bb
Justin Tunney <112041098+jtunney1@users.noreply.github.com>
2022-11-04 20:17:02 +0000

    Finished ch07 lab

--- ch07/lab/Rectangle.py
+++ ch07/lab/Rectangle.py
@@ -1,23 +1,18 @@
 class Rectangle:
   def __init__(self, x, y, h, w):
-    if x >= 0:
-      self.x = x
-    else:
+    self.x = x
+    self.y = y
+    self.height = h
+    self.width = w
+    if self.x < 0:
       self.x = 0
-    if y >= 0:
-      self.y = y
-    else:
+    if self.y < 0:
       self.y = 0
-    if h >= 0:
-      self.height = h
-    else:
+    if self.height < 0:
       self.height = 0
-    if w >= 0:
-      self.width = w
-    else:
+    if self.width < 0:
       self.width = 0
-
   def __str__(self):
-    string = f"(x: {self.x}, y: {self.y}, height: {self.height},width: {self.width}"
+    string = str.format("(x:{}, y:{}), height:{}, width{}".format(self.x,self.y, self.height,self.width))
     return string
 
